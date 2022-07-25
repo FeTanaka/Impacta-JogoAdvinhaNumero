b2e6d0300efa02c2b6f28a150200a47b4743f631
curso <curso@curso.com>
2022-07-25 20:25:54 -0300

    Códigos do Parabéns Fragment

--- app/src/main/java/br/com/impacta/jogoadvinhanumero/ParabensFragment.kt
+++ app/src/main/java/br/com/impacta/jogoadvinhanumero/ParabensFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import br.com.impacta.jogoadvinhanumero.databinding.FragmentParabensBinding
 
 class ParabensFragment : Fragment() {
+
+    private var _binding: FragmentParabensBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parabens, container, false)
+        _binding = FragmentParabensBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button4.setOnClickListener {
+            val action = ParabensFragmentDirections.actionParabensFragmentToChuteFragment()
+            findNavController().navigate(action)
+        }
     }
 
 }
